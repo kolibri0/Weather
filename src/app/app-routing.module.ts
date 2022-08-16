910cd354771c79902c1a071514fcd0fcfc995c77
kolibri0 <ddr5xxa@mail.ru>
2022-08-16 22:11:05 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForecastComponent } from './components/forecast/forecast.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SigninComponent } from './components/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'forecast', component: ForecastComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForecastComponent } from './components/forecast/forecast.component';
+import { ErrComponent } from './components/err/err.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ForecastComponent,
+    ErrComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
